cd0aea7ebeda9a0728d5b3945518f0cf9408fbf1
David Lodianto <david.lodianto@gmail.com>
2025-08-21 19:41:32 +0700

    fix: rename appcontroller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './public-page.controller';
+import { PublicPageController } from './public-page.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ModuleModule } from './module/module.module';
 import { CourseModule } from './course/course.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { ProtectedPagesController } from './protected-page.controller';
 
 @Module({
   imports: [UserModule, ModuleModule, CourseModule, AuthModule, CommonModule],
-  controllers: [AppController],
+  controllers: [PublicPageController, ProtectedPagesController],
   providers: [AppService],
 })
 export class AppModule {}
